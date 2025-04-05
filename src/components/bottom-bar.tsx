543d82b53891a22b2fe638c2a7772df6ecc45387
localhostd3veloper <gautamanand022@gmail.com>
2025-04-06 00:26:35 +0530

    feat: bottom bar

--- src/components/bottom-bar.tsx
+++ src/components/bottom-bar.tsx
@@ -1,6 +1,11 @@
 import {
+  VscBellDot,
+  VscBracketDot,
+  VscCheckAll,
   VscCloudUpload,
+  VscCopilot,
   VscError,
+  VscGitCommit,
   VscHistory,
   VscRemote,
   VscSourceControl,
@@ -9,12 +14,12 @@ import {
 
 export default function BottomBar() {
   return (
-    <div className="bg-sidebar text-xs border-t border-border text-white flex justify-between w-full  select-none h-6">
+    <div className="bg-sidebar text-xs border-t border-border text-gray-300 flex justify-between w-full  select-none h-6">
       <div className="flex gap-1">
         <div className="flex items-center justify-center px-2 bg-blue-500">
           <VscRemote size={16} />
         </div>
-        <div className="flex items-center justify-center gap-1">
+        <div className=" items-center justify-center gap-1 hidden md:flex">
           <VscSourceControl size={15} />
           <p>main*</p>
           <VscCloudUpload className="ml-2" size={15} />
@@ -25,7 +30,16 @@ export default function BottomBar() {
           </div>
         </div>
       </div>
-      <div className="ml-2"></div>
+      <div className="flex items-center gap-3 pr-4">
+        <VscGitCommit size={15} />
+        localhostd3veloper (45 mins ago)
+        <p className="flex items-center gap-1">Ln 23, Col 3</p>
+        UTF-8
+        <VscBracketDot size={15} /> Typescript JSX
+        <VscCopilot size={15} />
+        <VscCheckAll size={15} className="-mr-2" /> Prettier
+        <VscBellDot size={15} />
+      </div>
     </div>
   );
 }
