5ed70a381bfcd3ad6dfc8144e8ac3c8978c27624
localhostd3veloper <gautamanand022@gmail.com>
2025-04-05 17:15:26 +0530

    feat: main app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,50 @@
-import Image from "next/image";
+'use client';
+
+import { useState } from 'react';
+import PersistentSidebar from '@/components/persistent-sidebar';
+import TopBar from '@/components/top-bar';
+import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels';
+import BottomBar from '@/components/bottom-bar';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [terminalVisible, setTerminalVisible] = useState(true);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  return (
+    <div className="min-h-screen h-dvh bg-editor overflow-hidden flex flex-col">
+      <TopBar />
+
+      <PanelGroup direction="horizontal" className="flex-1">
+        <PersistentSidebar />
+        <Panel defaultSize={15} minSize={15} maxSize={30} collapsible>
+          <aside className="h-full w-full bg-sidebar">Files</aside>
+        </Panel>
+
+        <PanelResizeHandle className="w-0.5 bg-border hover:bg-token-keyword cursor-col-resize" />
+
+        <Panel minSize={40}>
+          <PanelGroup direction="vertical" className="h-full w-full">
+            <Panel defaultSize={80} minSize={30}>
+              <main className="h-full w-full bg-panel">editor area</main>
+            </Panel>
+
+            <PanelResizeHandle className="h-0.5 bg-border hover:bg-token-keyword cursor-row-resize" />
+
+            <Panel
+              defaultSize={20}
+              minSize={0}
+              collapsible
+              onResize={(size) => setTerminalVisible(size > 0)}
+            >
+              {terminalVisible && (
+                <div className="h-full w-full bg-sidebar text-white p-2">
+                  <div className="text-sm font-mono">Terminal</div>
+                </div>
+              )}
+            </Panel>
+          </PanelGroup>
+        </Panel>
+      </PanelGroup>
+      <BottomBar />
     </div>
   );
 }
