b0ee67d8e9e18c7c4564af5092804b9f3e6a6850
localhostd3veloper <gautamanand022@gmail.com>
2025-04-10 18:05:53 +0530

    format: page.tsx

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,28 +1,28 @@
-import { projectsData } from '@/constants/self';
 import React from 'react';
+import { projectsData } from '@/constants/self';
 
 export default function ProjectsPage() {
   return (
-    <div className="h-full w-full p-3 flex flex-col gap-2">
+    <div className="flex h-full w-full flex-col gap-2 p-3">
       <h1 className="text-2xl font-semibold">Projects</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {projectsData.map((project) => (
-          <div key={project.projectURL} className="p-4 border border-border">
+          <div key={project.projectURL} className="border-border border p-4">
             <img
               src={project.imageURLs[0]}
               alt={project.name}
               width={100}
               height={100}
-              className="w-full min-h-[100px] object-cover"
+              className="min-h-[100px] w-full object-cover"
             />
-            <h3 className="font-semibold text-lg mt-3">{project.name}</h3>
+            <h3 className="mt-3 text-lg font-semibold">{project.name}</h3>
             <p className="text-muted">
               {project.description}...{' '}
               <a
                 href={project.projectURL}
                 rel="noopener noreferrer"
                 target="_blank"
-                className="text-blue-500 hover:underline text-sm text-right"
+                className="text-right text-sm text-blue-500 hover:underline"
               >
                 Learn More
               </a>
