a3b9d6b5bdf8b8f164858c4b1d2da41cf5c43cbe
localhostd3veloper <gautamanand022@gmail.com>
2025-04-10 17:58:03 +0530

    format: about page

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,45 +1,44 @@
-import Image from 'next/image';
 import React from 'react';
-import TypewriterEffect from './components/typewriter-effect';
 import { socialMediaLinks } from '@/constants/self';
+import Image from 'next/image';
+
+import TypewriterEffect from './components/typewriter-effect';
 
 export default function AboutMePage() {
   return (
-    <div className="h-full p-4 md:p-20 flex flex-col gap-4 justify-center items-center">
-      <div className="flex items-center gap-6 flex-col">
+    <div className="flex h-full flex-col items-center justify-center gap-4 p-4 md:p-20">
+      <div className="flex flex-col items-center gap-6">
         <Image
           src="/profile.jpg"
           alt="Gautam Anand"
           width={300}
           height={300}
           quality={100}
-          className="rounded w-24 h-24 md:w-60 md:h-60 object-cover"
+          className="h-24 w-24 rounded object-cover md:h-60 md:w-60"
         />
-        <h1 className="text-2xl md:text-4xl font-semibold">Gautam Anand</h1>
+        <h1 className="text-2xl font-semibold md:text-4xl">Gautam Anand</h1>
         <div className="flex gap-4">
           {socialMediaLinks.map(({ icon: Icon, link }) => (
             <a href={link} key={link}>
-              <Icon className="w-6 h-6 hover:text-blue-500" />
+              <Icon className="h-6 w-6 hover:text-blue-500" />
             </a>
           ))}
         </div>
-        <div className="text-lg md:text-2xl font-medium">
+        <div className="text-lg font-medium md:text-2xl">
           I'm a <TypewriterEffect />
         </div>
-        <div className="text-lg md:text-2xl font-semibold">About Me</div>
+        <div className="text-lg font-semibold md:text-2xl">About Me</div>
       </div>
-      <div className="text-muted text-sm  text-center md:text-base max-w-3xl leading-relaxed tracking-wide">
-        Ever since I was a kid, I have been fascinated by technology and its
-        potential to change the world. I started learning programming when I was
-        15 years old and have been hooked ever since. Over the years, I have
-        worked on a wide range of projects, from simple websites to complex
-        applications. I am always eager to learn new technologies and stay up to
-        date with the latest trends in the industry.
+      <div className="text-muted max-w-3xl text-center text-sm leading-relaxed tracking-wide md:text-base">
+        Ever since I was a kid, I have been fascinated by technology and its potential to
+        change the world. I started learning programming when I was 15 years old and have
+        been hooked ever since. Over the years, I have worked on a wide range of projects,
+        from simple websites to complex applications. I am always eager to learn new
+        technologies and stay up to date with the latest trends in the industry.
         <br />
         <br />
-        In my free time, I enjoy playing video games, working out, and spending
-        time with friends and family. I also have a passion for singing and
-        photography.
+        In my free time, I enjoy playing video games, working out, and spending time with
+        friends and family. I also have a passion for singing and photography.
       </div>
     </div>
   );
